5e7dcd7ed1e32a5ce170ddd3088938165a841aa1
Erik Beebe <erik@beebe.cc>
2023-09-04 16:57:54 -0500

    add ctrl-d mapping to delete buffer in telescope

--- nvim/telescope_config.lua
+++ nvim/telescope_config.lua
@@ -1,4 +1,15 @@
 require('telescope').setup{
-  defaults = { sorting_strategy = "ascending" },
+  defaults = {
+    sorting_strategy = "ascending",
+    mappings = {
+          n = {
+            ['<c-d>'] = require('telescope.actions').delete_buffer
+          }, -- n
+          i = {
+            ["<C-h>"] = "which_key",
+            ['<c-d>'] = require('telescope.actions').delete_buffer
+          } -- i
+        }, --mappings
+  },
   pickers = { find_files = { hidden = true } }
 }
