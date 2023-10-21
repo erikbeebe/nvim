b99d78896507df964930344eacae7df8e81eac51
Erik Beebe <erik@beebe.cc>
2023-10-21 10:09:23 -0500

    attempt to re-use existing tabs/buffers in telescope

--- nvim/telescope_config.lua
+++ nvim/telescope_config.lua
@@ -1,3 +1,4 @@
+local actions = require("telescope.actions")
 require('telescope').setup{
   defaults = {
     sorting_strategy = "ascending",
@@ -12,5 +13,27 @@ require('telescope').setup{
           } -- i
         }, --mappings
   },
-  pickers = { find_files = { hidden = true } }
+  pickers = {
+    find_files = {
+      hidden = true,
+      mappings = {
+        i = { ["<CR>"] = actions.select_tab_drop }
+      }
+    },
+    buffers = {
+      mappings = {
+        i = { ["<CR>"] = actions.select_tab_drop }
+      }
+    },
+    git_files = {
+      mappings = {
+        i = { ["<CR>"] = actions.select_tab_drop }
+      }
+    },
+    old_files = {
+      mappings = {
+        i = { ["<CR>"] = actions.select_tab_drop }
+      }
+    }
+  }
 }
